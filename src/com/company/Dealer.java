8b7dc8e80b9086b0a361a85a42f5ae3b2fd3171d
Fedor Lyanguzov <fedor.lyanguzov@ya.ru>
2016-11-14 18:57:35 +0300

    14.11.2016 Deck, Hand, Player: Human, Computer->Dealer.

---
+++ src/com/company/Dealer.java
@@ -0,0 +1,8 @@
+package com.company;
+
+/**
+ * Created by falyanguzov on 14.11.2016.
+ */
+public class Dealer extends Computer {
+    Deck deck = new Deck();
+}
---
+++ src/com/company/Player.java
@@ -0,0 +1,8 @@
+package com.company;
+
+/**
+ * Created by falyanguzov on 14.11.2016.
+ */
+public abstract class Player {
+    Hand hand = new Hand();
+}
